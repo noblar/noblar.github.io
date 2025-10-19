a6d93157715fe1b92d7a4482c76b2c227f03eea9
Alexandru Gaman <alex@fogwarts.dev>
2025-10-19 19:16:20 +0300

    Initial website variant

---
+++ index.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    console.log('TCFG')
+
+    tailwind.config = {
+        theme: {
+            extend: {
+                colors: {
+                    background: '#0d0d0d',
+                    text: '#f5f5f5',
+                    accent: '#22d3ee'
+                }
+            }
+        }
+    }
+}
